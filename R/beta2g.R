15faad1a652684cac59f8aa7df912121b295f010
Cristian Opariuc-Dan <copariuc@gmail.com>
2022-02-17 08:27:28 +0200

    Update

--- R/beta2g.R
+++ R/beta2g.R
@@ -7,10 +7,10 @@ library(esc)
 #'
 beta2es <-function(study, type, beta, sd, n1, n2, X, Y, Rxx = 0, Ryy = 0){
   g <- esc::esc_beta(beta = beta, sdy = sd, grp1n = n1, grp2n = n2, es.type = type, study = study)
-  g <- data.frame(c(study, g$es, g1$w, g$totaln, g$se, g$var, g$ci.lo, g$ci.hi, g$measure,
+  g <- data.frame(c(study, g$es, g$w, g$totaln, g$se, g$var, g$ci.lo, g$ci.hi, g$measure,
                     X, Rxx, Y, Ryy))
   g <- as.data.frame(t(g)); rownames(g) <- NULL
-  #colnames(g) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
-  #                      "X", "Rxx", "Y", "Ryy")
+  colnames(g) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
+                        "X", "Rxx", "Y", "Ryy")
   return(g)
 }
