bf47250abb89aff7708b1780bf71a7a76e618527
Cristian Opariuc-Dan <copariuc@gmail.com>
2022-02-17 08:25:55 +0200

    update

--- R/beta2g.R
+++ R/beta2g.R
@@ -10,7 +10,7 @@ beta2es <-function(study, type, beta, sd, n1, n2, X, Y, Rxx = 0, Ryy = 0){
   g <- data.frame(c(study, g$es, g1$w, g$totaln, g$se, g$var, g$ci.lo, g$ci.hi, g$measure,
                     X, Rxx, Y, Ryy))
   g <- as.data.frame(t(g)); rownames(g) <- NULL
-  colnames(g) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
-                        "X", "Rxx", "Y", "Ryy")
+  #colnames(g) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
+  #                      "X", "Rxx", "Y", "Ryy")
   return(g)
 }
