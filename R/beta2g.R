307087842312afd24c35f990f77897c7a1af28e5
Cristian Opariuc-Dan <copariuc@gmail.com>
2022-02-21 18:34:39 +0200

    Update

--- R/beta2g.R
+++ R/beta2g.R
@@ -5,14 +5,16 @@ library(esc)
 #' @description
 #' This function converts form a beta coefficient to an effect size object
 #'
-beta2es <-function(study, type, beta, sd, n1, n2, X, Y, Rxx, Ryy){
-  rec <- 1; es <- data.frame(study = NA, es = NA, weight = NA, sample.size = NA, se = NA, var = NA,
-                             ci.lo = NA, ci.hi = NA, measure = NA, X = NA, Rxx = NA, Y = NA, Ryy = NA)
-  while(rec <= length(beta)){
-    g <- esc::esc_beta(beta = beta[rec], sdy = sd[rec], grp1n = n1, grp2n = n2, es.type = type, study = study)
+beta2es <-function(Study, Type, Beta, SDy, N1, N2, Y, X, Ryy, Rxx){
+  recY <- 1; recX <- 1
+  es <- data.frame(study = NA, es = NA, weight = NA, sample.size = NA, se = NA, var = NA,
+                   ci.lo = NA, ci.hi = NA, measure = NA, X = NA, Rxx = NA, Y = NA, Ryy = NA)
+  while(rec <= length(Beta)){
+    g <- esc::esc_beta(beta = Beta[rec], sdy = SDy[rec], grp1n = N1, grp2n = N2, es.type = Type, study = Study)
     # Binding all data
-    es <- rbind(es, c(study, g$es, g$w, g$totaln, g$se, g$var, g$ci.lo, g$ci.hi, g$measure, X, Rxx, Y, Ryy))
-    rec <- rec +1
+    es <- rbind(es, c(Study, g$es, g$w, g$totaln, g$se, g$var, g$ci.lo, g$ci.hi, g$measure,
+                      X[rec], Rxx[rec], Y[rec], Ryy[rec]))
+    rec <- rec + 1
   }
   colnames(g) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
                         "X", "Rxx", "Y", "Ryy");  return(g)
