24c320c3f8138cd1f834a41f4fe5d9309cbcc5dd
Cristian Opariuc-Dan <copariuc@gmail.com>
2022-02-21 19:11:11 +0200

    Update

--- R/beta2g.R
+++ R/beta2g.R
@@ -15,6 +15,14 @@ beta2es <-function(Study, Type, Beta, SDy, N1, N2, Y, X, Ryy, Rxx){
                       X[rec], Rxx[rec], Y[rec], Ryy[rec]))
     rec <- rec + 1
   }
-  colnames(g) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
+  colnames(es) <- c("study", "es", "weight", "sample.size", "se", "var", "ci.lo", "ci.hi", "measure",
                         "X", "Rxx", "Y", "Ryy");  return(g)
 }
+
+# beta <- c(1.00, .01, .01)
+# sd <- c(0.06 * sqrt(160), 0.05 * sqrt(160), 0.05 * sqrt(160))
+# Y <- c(rep("Harmfull", 3)); X <- c("Psychopathy", "Machiavellianism", "Narcissism")
+# Ryy <- c(rep(NA, 3)); Rxx <- c(rep(.72, 3))
+#
+# temp <- beta2es(Study = "dem$author", Type = "g", Beta = beta, SDy = sd,
+#                 N1 = 87, N2 = 87, Y = Y, X = X, Ryy = Ryy, Rxx = Rxx)
